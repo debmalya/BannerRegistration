32a18343bef06ae6fc16571b5e9fa0d3ea9c6d14
Debmalya Jash <debmalya.jash@gmail.com>
2015-03-03 19:51:55 +0800

    Add more comments about the method. What it should do?

--- BannerRegistration/src/main/java/BannerRegistration.java
+++ BannerRegistration/src/main/java/BannerRegistration.java
@@ -1,4 +1,3 @@
-
 public class BannerRegistration
 {
 
@@ -19,13 +18,23 @@ public class BannerRegistration
   }
 
 
+
   /**
-   * information extraction from textual data.
+   * The United States National Institute of Health (NIH) has built a system
+   * that uses expert labeling to annotate abstracts from Pubmed so disease
+   * characteristics can be more easily identified. This open-source, supervised
+   * learning system called BANNER achieves a good level of prediction power.
+   * 
+   * Information extraction from textual data. To aid in the automated Named
+   * Entity Recognition (NER) of biomedical publications. Learn to accurately
+   * merge data collected from multiple annotators of varying quality and
+   * integrate this data into predictive models.
+   * 
    * @param s
    * @return
    */
   public String displayTestCase (String s)
-  { 
+  {
     String result = null;
     return result;
   }
