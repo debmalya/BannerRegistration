6cba672125fe5d9b2576ff16681c86db58840729
Debmalya Jash <debmalya.jash@gmail.com>
2015-03-03 16:32:46 +0800

    Initial Commit

---
+++ BannerRegistration/test/main/java/BannerRegistration.java
@@ -0,0 +1,28 @@
+
+public class BannerRegistration
+{
+
+  public BannerRegistration ()
+  {
+    // TODO Auto-generated constructor stub
+  }
+
+
+
+  /**
+   * @param args
+   */
+  public static void main (String[] args)
+  {
+    // TODO Auto-generated method stub
+
+  }
+
+
+
+  public String displayTestCase (String s)
+  { 
+    String result = null;
+    return result;
+  }
+}
