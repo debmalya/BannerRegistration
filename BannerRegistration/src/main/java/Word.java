ce1e033242e81c51dfea8fb8910f0fd6f201218a
Debmalya Jash <debmalya.jash@gmail.com>
2015-03-08 12:34:54 +0800

    Added two new files FrequenceCounter.java and Word.java

---
+++ BannerRegistration/src/main/java/Word.java
@@ -0,0 +1,12 @@
+
+
+import java.util.List;
+
+public class Word {
+	int totalCount;
+	/**
+	 * This will have position in the sentence / length of the sentence.
+	 * Normally entities are either first part or in last part of the sentence.
+	 */
+	List<Float> positionList;
+}
